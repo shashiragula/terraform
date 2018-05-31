b8d398923ef758e852c78dfd64782172e71da36a
EC2 Default User <ec2-user@ip-172-31-27-58.ec2.internal>
2018-05-31 05:11:32 +0000

    adding vpc configs and packer ubuntu image template

---
+++ aws/networking/main.tf
@@ -0,0 +1,43 @@
+#---------networking/main.tf-----------
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "tf_vpc" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    
+    tags {
+        Name = "tf_vpc"
+    }
+}
+
+resource "aws_internet_gateway" "tf_internet_gateway" {
+    vpc_id = "${aws_vpc.tf_vpc.id}"
+    
+    tags {
+        Name = "tf_igw"
+    }
+}
+
+
+resource "aws_route_table" "tf_public_rt" {
+    vpc_id = "${aws_vpc.tf_vpc.id}"
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.tf_internet_gateway.id}"
+    }
+    
+    tags {
+        Name = "tf_public_rt"
+    }
+}
+
+resource "aws_default_route_table" "tf_private_rt" {
+    default_route_table_id = "${aws_vpc.tf_vpc.default_route_table_id}"
+    
+    tags {
+        Name = "tf_private_rt"
+    }
+}
---
+++ aws/networking/variables.tf
@@ -0,0 +1,5 @@
+#---------networking/variables.tf-----------
+
+variable "vpc_cidr" {
+    default = "10.111.0.0/16"
+}
